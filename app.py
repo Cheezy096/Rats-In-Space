9048fe35ad1f8875613610f17e29458748af25e4
Jake <thecheese096@gmail.com>
2021-12-26 22:32:02 +0000

    Big stuff
    I have not been properly tracking these files so this is going to be an annoyingly big commit. 
    
    Checklist for stuff that has been changed or modified or removed:
    
    - A render function has been made to act as a way to redirect the user to a different version of the site depending on the platform they are on OR if they are using a beta version of the frontend.
    
    - A way to access the beta frontend has been added
    
    - A user system has been added taking use of the SQL database (currently passwords are unencrypted, this needs to be resolved soon)
    
    - Methods of registering an account, logging in, logging out and detecting if the user is logged in before doing anything in these functions have been added
    
    - The frontend now shows buttons and text (IE: "Welcome, {user name}" depending on if they are logged in or not
    
    - An ability to delete boards have been added
    
    - Removed the "home" route
    
    - A check has been implemented into the boards route to make sure the board actually exists in the database, if it doesn't we simply return a 404
    
    - The "allposts" route has now been replaced with a "userlist" route making use of the new user system
    
    - 505 and 401 response handling has been added and now sends you to an appropriate page
    
    - admin_only check has been implemented to make sure that no one with a type of 0 (regular users) can just delete boards at free will

--- app.py
+++ app.py
@@ -1,34 +1,173 @@
-import json, sqlite3, builtins, random
+import json, sqlite3, builtins, random, jinja2
+from functools import wraps
 from flask import Flask, redirect, url_for, render_template, request, session, abort
 from datetime import timedelta
 from etc import database
 
 database.connect()
-print("no connect why")
 app = Flask(__name__)
 app.secret_key = "iamakey"
 
 r = 2
 builtins.randomAdFoot = [["Do you like spaghetti? I sure do!", "Check out Marisa Space for some spaghetti!", "http://chiyo.org/"], ["The best content platform around!","VidLii!", "https://vili.co/"], ["Miss the days of realism in art? Worry no more!", "Hyper-realistic Sonic is here to save us!", "https://c.tenor.com/9z3rpvYfoDIAAAAM/sonic-and-mario-kiss.gif"]] # Turn this into JSON file
 
-@app.route("/")
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kws):
+            try:
+                if session["type"] == 1:
+                    pass
+                else:
+                    abort(401)
+            except KeyError:
+                abort(401)
+            return f(*args, **kws)            
+    return decorated_function
+
+def render(temp=None, **kwargs):
+    if not temp: return "What the hell am i gonna render without a temlate!"
+
+    agent = request.headers.get('User-Agent')
+    phones = ["iphone", "android", "blackberry"]
+    
+    try:
+        if session["beta"]:
+            return render_template("cosmic/" + temp, **kwargs)
+    except KeyError:
+        pass
+    except jinja2.exceptions.TemplateNotFound:
+        return render_template("pc/" + temp, **kwargs)
+
+    try:
+        if any(phone in agent.lower() for phone in phones):
+            return render_template("ph/" + temp, **kwargs)
+        else:
+            return render_template("pc/" + temp, **kwargs)
+    except jinja2.exceptions.TemplateNotFound:
+        abort(404)
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
-    boards = cursor.execute("SELECT * FROM `boards`").fetchall()
-    return render_template("index.html", boards=boards, adFoot=randomAdFoot)
+    if request.method == "POST":
+        print(request.form["submit"])
+    try:
+        boards = cursor.execute("SELECT * FROM `boards`").fetchall()
+        users = cursor.execute("SELECT * FROM users").fetchall()
+        return render("index.html", boards=boards, users=users, adFoot=randomAdFoot, session=session)
+    except sqlite3.ProgrammingError:
+        return "PISS"
 
-@app.route("/test/<name>") # Test me world! <h1>tagger</h1>
-def home(name):
-    return render_template("tagger.html", content=name, r=r, list=["rattim","ratjohnson","ratjunior"])
+@app.route("/beta")
+def beta():
+    try:    
+        session.pop("beta")
+    except KeyError:
+        session["beta"] = 1
 
-@app.route("/newboard", methods=["GET", "POST"])
-def new_board():
+    return redirect(url_for("index"))
+
+#    return render("tagger.html", content=name, r=r, list=["rattim","ratjohnson","ratjunior"])
+
+@app.route("/logout")
+def logout():
+    try:
+        if session["username"]:
+            [session.pop(key) for key in list(session.keys())]
+    except KeyError:
+        pass
+
+    return redirect(url_for("index"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
     randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
-    print("newboard")
+
+    try:
+        if session["username"]:
+            return redirect(url_for("index"))
+    except KeyError:
+        pass
 
     if request.method == "GET":
-        return render_template("newboard.html", adFoot=randomAdFoot)
-        print("newboard")
+        return render("login.html", dumbname=False, dumbpass=False, adFoot=randomAdFoot)
+    else:
+        userInfo = cursor.execute("SELECT * from `users` WHERE username = ?", (request.form["username"],)).fetchone()
+        if not userInfo: 
+            return render("login.html", dumbname=True, dumbpass=False, adFoot=randomAdFoot)
+
+        if userInfo[1] != request.form["password"]:
+            return render("login.html", dumbname=False, dumbpass=True, adFoot=randomAdFoot)
+
+        session["username"] = userInfo[0]
+        session["id"] = userInfo[2]
+        session["type"] = userInfo[3]
+
+    return redirect(url_for("index"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
+
+    try:
+        if session["username"]:
+            return redirect(url_for("index"))
+    except KeyError:
+        pass
+
+    if request.method == "GET":
+        return render("register.html", dumbname=False, dumbpass=False, adFoot=randomAdFoot)
+    else:
+
+        userInfo = cursor.execute("SELECT * from `users` WHERE username = ?", (request.form["username"],)).fetchone()
+        if userInfo: 
+            return render("register.html", dumbname=True, dumbpass=False, adFoot=randomAdFoot)
+        
+        if len(request.form["password"].split()) == 0:
+            return render("register.html", dumbname=False, dumbpass=True, adFoot=randomAdFoot)
+
+
+        userID = cursor.execute("SELECT MAX(id) FROM `users`").fetchone()[0]
+        if not userID:
+            if userID == 0:
+                pass
+            else:
+                userID = -1
+        
+        cursor.execute("INSERT INTO users(username, password, id, type) VALUES(?,?,?,?)", (request.form["username"], request.form["password"], userID + 1, 0,))
+        sql.commit()
+
+        userInfo = cursor.execute("SELECT * from `users` WHERE username = ?", (request.form["username"],)).fetchone()
+
+        session["username"] = userInfo[0]
+        session["id"] = userInfo[2]
+        session["type"] = userInfo[3]
+        print(userInfo)
+    return redirect(url_for("index"))
+
+@app.route("/delboard/<board>/", methods=["GET", "POST"])
+@admin_only
+def delete_board(board):
+    userCheck = cursor.execute("SELECT `type` FROM `users` WHERE username = ?", (session["username"],)).fetchone()
+    if not userCheck: return "bad"
+
+    print(userCheck[0])
+    if userCheck[0] == 1:
+        cursor.execute("DELETE FROM `boards` WHERE board_id = ?", (board,)).fetchone()
+        cursor.execute("DELETE FROM `threads` WHERE board_id = ?", (board,)).fetchone()
+        cursor.execute("DELETE FROM `posts` WHERE board_id = ?", (board,)).fetchone()
+    sql.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/newboard", methods=["GET", "POST"])
+@admin_only
+def new_board():
+    randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
+
+    if request.method == "GET":
+        return render("newboard.html", adFoot=randomAdFoot)
     else:
         boardID = cursor.execute("SELECT MAX(board_id) FROM `boards`").fetchone()[0]
         if not boardID:
@@ -46,21 +185,32 @@ def board(board):
 
     boards = cursor.execute("SELECT * FROM boards WHERE board_id = ?", (board,)).fetchone()
     threads = cursor.execute("SELECT * FROM threads WHERE board_id = ?", (board,)).fetchall()
+    users = cursor.execute("SELECT * FROM users").fetchall()
 
-    return render_template("board.html", thread=threads, board=boards, adFoot=randomAdFoot)
+    if not boards:
+        abort(404)
 
+    return render("board.html", thread=threads, board=boards, users=users, adFoot=randomAdFoot)
 
+@app.route("/help")
+def help():
+    randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
+
+    return render("help.html", adFoot=randomAdFoot)
 
 @app.route("/b/<board>/<thread>")
 def thread(board, thread):
     randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
+    
     boards = cursor.execute("SELECT * FROM boards WHERE board_id = ?", (board,)).fetchone()
     threads = cursor.execute("SELECT * FROM threads WHERE thread_id = ?", (thread,)).fetchone()
     posts = cursor.execute("SELECT * FROM posts WHERE thread_id = ?", (thread,)).fetchall()
+    users = cursor.execute("SELECT * FROM users").fetchall()
 
-    return render_template("thread.html", thread=threads, board=boards, post=posts, adFoot=randomAdFoot)
+    if not threads:
+        abort(404)
 
-
+    return render("thread.html", thread=threads, board=boards, post=posts, users=users, adFoot=randomAdFoot)
 
 @app.route("/b/<board>/<thread>/post", methods=["GET", "POST"])
 def new_thread_comment(board, thread):
@@ -76,8 +226,6 @@ def new_thread_comment(board, thread):
 
     return redirect(url_for("thread", board=board, thread=thread))
 
-
-
 @app.route("/b/<board>/post", methods=["GET", "POST"])
 def new_thread(board):
     threadID = cursor.execute("SELECT MAX(thread_id) FROM `threads`").fetchone()[0]
@@ -92,15 +240,12 @@ def new_thread(board):
 
     return redirect(url_for("board", board=board))
 
-
-@app.route("/allposts")
-def allposts():
+@app.route("/users")
+def userlist():
     randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
-    boards = cursor.execute("SELECT * FROM boards").fetchall()
-    threads = cursor.execute("SELECT * FROM threads").fetchall()
+    users = cursor.execute("SELECT * FROM users").fetchall()
 
-    return render_template("allthreads.html", posts=threads, boards=boards, adFoot=randomAdFoot)
-
+    return render("userlist.html", users=users, adFoot=randomAdFoot)
 
 @app.route("/random")
 def randomBoard():
@@ -111,8 +256,18 @@ def randomBoard():
 
 @app.errorhandler(404)
 def page_not_found(a):
-    randomAdFoot = builtins.randomAdFoot[random.randint(0, len(builtins.randomAdFoot) - 1)]
-    return render_template('index.html',adFoot=randomAdFoot), 404
+    return render_template("errors/404.html")
+    #return redirect(url_for("index"))
+
+@app.errorhandler(500)
+def internal_server_error(a):
+    return render_template("errors/500.html")
+    #return redirect(url_for("index"))
+
+@app.errorhandler(401)
+def forbidden(e):
+    return render_template("errors/401.html")
 
 if __name__ == "__main__":
-    app.run()
+    while True:
+        app.run(debug=True, host="0.0.0.0")
